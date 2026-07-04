2f1804c596527d77a622e0b3042b3efacd1cba70
TejalNPowar <tejalpowar53@gmail.com>
2026-07-04 23:40:52 +0530

    feat: implement persistent login with JWT

--- server/server.js
+++ server/server.js
@@ -7,16 +7,20 @@ const authRoutes = require("./routes/authRoutes");
 const swapRoutes = require("./routes/swapRoutes");
 
 const app = express();
-app.use(express.json());
-connectDB();
-
-app.use("/api/auth", authRoutes);
-app.use("/api/swaps", swapRoutes);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
+app.use(express.json());
 
+connectDB();
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/swaps", swapRoutes);
 
 // Default Route
 app.get("/", (req, res) => {
@@ -25,8 +29,6 @@ app.get("/", (req, res) => {
 
 // Server Port
 const PORT = process.env.PORT || 5000;
-
-
 
 // Start Server
 app.listen(PORT, () => {
